1ebc191fee039c69012aa925acabfa72e0280b06
Daniil Shenyagin <d.shenyagin@tinkoff.ru>
2023-04-22 18:20:56 +0300

    added new event

--- socket/quiz/events.ts
+++ socket/quiz/events.ts
@@ -1,3 +1,5 @@
 export const event_quiz_get_next_card = "quiz.get_next_card";
 export const event_quiz_set_answer = "quiz.set_answer";
 export const event_quiz_skip_card = "quiz.skip_card";
+
+export const event_quiz_end_test = "quiz.end_test";
