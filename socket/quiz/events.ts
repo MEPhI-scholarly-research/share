806183a423149756265effb62aa3149636908937
Daniil Shenyagin <d.shenyagin@tinkoff.ru>
2023-04-23 16:49:28 +0300

    added init card event

--- socket/quiz/events.ts
+++ socket/quiz/events.ts
@@ -1,3 +1,4 @@
+export const event_quiz_init_card = "quiz.init_card";
 export const event_quiz_get_next_card = "quiz.get_next_card";
 export const event_quiz_set_answer = "quiz.set_answer";
 export const event_quiz_skip_card = "quiz.skip_card";
